d6f936b273b41722f724216b2e8491bb1d1f7a06
lenane57 <65447660+lenane57@users.noreply.github.com>
2021-05-14 12:53:53 +0100

    changed made

--- DateTime.php
+++ DateTime.php
@@ -8,9 +8,11 @@ echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l"."<br>");
+
 // Return date/time info of a timestamp; then format the output
 $mydate=getdate(date("U"));
-echo "$mydate[weekday], $mydate[month] $mydate[mday], $mydate[year]";
+
+echo "<br> $mydate[month] $mydate[mday], $mydate[year]";
 
 ?>
 
