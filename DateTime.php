198b80a770faf2b25265f6f4b5ed1babc013a45b
lenane57 <65447660+lenane57@users.noreply.github.com>
2021-05-14 12:36:24 +0100

    added more PHP code to Date time folder
    added my full name to License to cover myself

--- DateTime.php
+++ DateTime.php
@@ -3,10 +3,15 @@
 <body>
 
 <?php
+// Return today date.
 echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
-echo "Today is " . date("l");
+echo "Today is " . date("l"."<br>");
+// Return date/time info of a timestamp; then format the output
+$mydate=getdate(date("U"));
+echo "$mydate[weekday], $mydate[month] $mydate[mday], $mydate[year]";
+
 ?>
 
 </body>
